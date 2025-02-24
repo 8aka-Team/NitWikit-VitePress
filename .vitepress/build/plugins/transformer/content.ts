771e19e44727f4b052cc007d8d8fffe9891d6288
evenwan <evenwan0010@163.com>
2025-02-24 09:31:12 +0800

    feat: 兼容不合规的style标签

--- .vitepress/build/plugins/transformer/content.ts
+++ .vitepress/build/plugins/transformer/content.ts
@@ -2,7 +2,8 @@ import { BasePlugin } from "../base";
 
 export class NitWikitContentTransformer extends BasePlugin {
   static #regExp: RegExp = /^(---([\s\S]*)---)?([\r|\n]*)(?<heading>[#]{2,6} )(?<title>[\s\S].*)/;
-  static #imgSrcRegExp: RegExp = /\<img(.*)src="(?<src>_images\/(.*))"/
+  static #imgSrcRegExp: RegExp = /\<img(.*)src="(?<src>_images\/(.*))"/;
+  static #styleAttrRegExp: RegExp = /style={{(?<val>.*)}}/;
 
   constructor() {
     super({
@@ -16,15 +17,30 @@ export class NitWikitContentTransformer extends BasePlugin {
       const result = code.match(NitWikitContentTransformer.#regExp);
       let newCode = code;
       if (result !== null) {
-        newCode = code.replace(`${result.groups?.heading}${result.groups?.title}`, `# ${result.groups?.title}`);        
+        newCode = code.replace(`${result.groups?.heading}${result.groups?.title}`, `# ${result.groups?.title}`);
       }
 
       const imgResult = code.match(new RegExp(NitWikitContentTransformer.#imgSrcRegExp, "g"));
       if (imgResult !== null) {
-        imgResult.forEach(img => {
+        imgResult.forEach((img) => {
           const tmp = img.match(NitWikitContentTransformer.#imgSrcRegExp);
-          if(tmp){
-            newCode = newCode.replace(tmp.groups!.src,`./${tmp.groups!.src}`);        
+          if (tmp) {
+            newCode = newCode.replace(tmp.groups!.src, `./${tmp.groups!.src}`);
+          }
+        });
+      }
+
+      /**
+       * 兼容不合规的style标签
+       * <img src="https://example.com/bar.png" width="45%" style={{marginRight: '20px'}} />
+       */
+      const styleAttrimgResult = code.match(new RegExp(NitWikitContentTransformer.#styleAttrRegExp, "g"));
+      if (styleAttrimgResult !== null) {
+        styleAttrimgResult.forEach((img) => {
+          const tmp = img.match(NitWikitContentTransformer.#styleAttrRegExp);
+          if (tmp) {
+            const finalVal = tmp.groups!.val.replaceAll(",", ";").replace(/[\'|\"]/g, "");
+            newCode = newCode.replace(`{{${tmp.groups!.val}}}`, `"${finalVal}"`);
           }
         });
       }
